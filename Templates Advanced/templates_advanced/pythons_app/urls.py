d0ac86b25add6b0c50ea5a2383773cba403305ec
Asen Kamboshev <lion963@mail.bg>
2021-07-12 12:52:41 +0300

    corrections3_12.07

--- Templates Advanced/templates_advanced/pythons_app/urls.py
+++ Templates Advanced/templates_advanced/pythons_app/urls.py
@@ -2,14 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('create/', views.create, name="create"),
+    # path('', views.index, name="index"),
+    path('', views.IndexView.as_view(), name='index'),
+    # path('create/', views.create, name="create"),
+    path('create/', views.CreatePythonView.as_view(), name='create'),
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
+    # path('register/', views.RegisterView.as_view(), name='register'),
     path('logout/', views.logout_view, name='logout'),
     path('page_401', views.page_401, name='page 401'),
     path('profile', views.profile_view, name='profile'),
     path('update', views.update_profile, name='update'),
-
 
 ]
