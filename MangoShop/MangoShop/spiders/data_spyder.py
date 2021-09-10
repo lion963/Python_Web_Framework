919952d28c17f546a3b26922dd16eea3d30537ee
Asen Kamboshev <71500381+lion963@users.noreply.github.com>
2021-09-11 00:29:31 +0300

    scrapy project

--- MangoShop/MangoShop/spiders/data_spyder.py
+++ MangoShop/MangoShop/spiders/data_spyder.py
@@ -5,10 +5,10 @@ class DataSpider(scrapy.Spider):
     start_urls =['https://shop.mango.com/bg-en/women/skirts-midi/midi-satin-skirt_17042020.html?c=99',]
 
     def parse(self, response, **kwargs):
-        name = response.css('.product-name::text').get()
-        price = response.css('.product-sale::text').get()
-        color = response.css('.colors-info-name::text').get()
-        size = response.css('.selector-trigger::text').get()
+        name = response.css('h1.product-name::text').get()
+        price = response.css('span.product-sale::text').get()
+        color = response.css('span.colors-info-name::text').get()
+        size = response.css('span.selector-trigger::text').get()
 
         yield {
             'name': name,
